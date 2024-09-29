edf3acba7fac4c97cef65ce0d5511eca2cda0780
Joshua <joshjosh530@gmail.com>
2024-09-29 12:08:56 +0100

    Added While loop

---
+++ JavaFundemntalesQuiz/src/WhileLoop.java
@@ -0,0 +1,12 @@
+public class WhileLoop {
+    public static void main(String[] args) {
+        // Initialize the counter
+        int i = 1;
+
+        // While loop to print numbers from 1 to 10
+        while (i <= 10) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
