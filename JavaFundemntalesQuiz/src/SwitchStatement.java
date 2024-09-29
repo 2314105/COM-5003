a7a251cf5d4f313992e81b1804801505f0e3a7e8
Joshua <joshjosh530@gmail.com>
2024-09-29 12:11:21 +0100

    Added Switch Statement

---
+++ JavaFundemntalesQuiz/src/SwitchStatement.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SwitchStatement {
+
+    public static void main(String[] args) {
+        // Create a scanner object to read input
+        Scanner scanner = new Scanner(System.in);
+
+        // Ask for user input
+        System.out.print("Enter a number ( 1 - 3 ): ");
+        int day = scanner.nextInt();
+
+        // Use a switch statement to determine the day of the week
+        switch (day) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            default:
+                System.out.println("Invalid input");
+        }
+
+        // Close the scanner
+        scanner.close();
+    }
+}
