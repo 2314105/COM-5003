8fdc699f7952798a599e568f25384fbade3b93f3
Joshua <joshjosh530@gmail.com>
2024-10-03 12:00:38 +0100

    added boolean to personal fan

--- Fan/src/PersonalFan.java
+++ Fan/src/PersonalFan.java
@@ -7,6 +7,7 @@ public class PersonalFan {
         String border = "<------------------------------------------------>";
         char symbol = '>';
         int userOption = 0;
+        boolean isFanOn = false; // Boolean to check if the fan is on or off
         String fanOptions = "\t0 - OFF\n\t1 - Speed 1\n\t2 - Speed 2\n\t3 - Speed 3\n\t4 - Speed 4\n\t5 - Speed 5";
 
         // Main loop to control fan settings
@@ -20,21 +21,27 @@ public class PersonalFan {
 
             switch (userOption) {
                 case 0:
+                    isFanOn = false;
                     System.out.println("Fan Off");
                     break;
                 case 1:
+                    isFanOn = true;
                     System.out.println("Fan Speed = 1");
                     break;
                 case 2:
+                    isFanOn = true;
                     System.out.println("Fan Speed = 2");
                     break;
                 case 3:
+                    isFanOn = true;
                     System.out.println("Fan Speed = 3");
                     break;
                 case 4:
+                    isFanOn = true;
                     System.out.println("Fan Speed = 4");
                     break;
                 case 5:
+                    isFanOn = true;
                     System.out.println("Fan Speed = 5");
                     break;
                 default:
@@ -42,7 +49,14 @@ public class PersonalFan {
                     break;
             }
 
-            // Exit the loop if fan is turned off
+            // Print the current fan state
+            if (isFanOn) {
+                System.out.println("The fan is currently ON.");
+            } else {
+                System.out.println("The fan has been turned OFF.");
+            }
+
+            // Exit the loop if fan is turned off (userOption == 0)
             if (userOption == 0) {
                 break;
             }
